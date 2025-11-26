c9c4d7ca2c44c97c1069426653b6507593a72502
mmesomaalagwu@gmail.com <mmesomaalagwu@gmail.com>
2025-11-26 16:54:03 +0100

    still testing modifications

--- app.py
+++ app.py
@@ -319,7 +319,6 @@ def volume_down():
 
 if __name__ == "__main__":
     with app.app_context():  
-        # db.drop_all()  
         db.create_all()
     app.run( host='localhost', port=5000, debug = True)
 
